cf58d4303edfefab8620f794908366e8ab507cfe
mmd-oetama <asfaindonesia123@gmail.com>
2025-10-23 10:09:59 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+//1. pilih tombol dengan IDnya
+const tombolSapa = document.getElementyId('sapaButton');
+//2. Tambahkan event listener untuk aski click
+tombolSapa.addEventListener('click', function() {
+  //3. Jalankan kode ini ketika di click
+  alert('Terimakasih sudah berkunjung!);
+});
